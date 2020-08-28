114658b04fbcf9c09ea4a9f63def863554a614be
Miguel Heinen <61639683+Hipoacusia@users.noreply.github.com>
2020-08-28 15:53:01 -0500

    Update Proyectos.js

--- Blog Hipoacusia/public_html/Proyectos.js
+++ Blog Hipoacusia/public_html/Proyectos.js
@@ -1,14 +1,36 @@
 document.querySelector('#player').addEventListener("mouseover",sumarPuntos);
 
-puntos = 0;
+var puntos = 0;
+var tiempo = 60;
+var necesarios = 30;
+
 function sumarPuntos(){
-    puntos = puntos + 1;
-    document.querySelector(".PointerB").innerHTML = "Puntos: <b>" + puntos + "</b>";
+      puntos ++;
+      document.querySelector("#Puntos").innerHTML = 
+      "Puntos: <b>" + puntos + "/"  + necesarios + "</b>";
   
   RadnNum = Math.round(Math.random()*540);
   RadnNum2 = Math.round(Math.random()*335);
-  document.querySelector('#player').style.marginTop = RadnNum + "px";
-  document.querySelector('#player').style.marginLeft = RadnNum2 + "px";
+    document.querySelector('#player').style.marginTop = RadnNum + "px";
+    document.querySelector('#player').style.marginLeft = RadnNum2 + "px";
+    if(puntos == 30){
+      alert("Ganaste Perrito maquinola");
+      tiempo = 60;
+      puntos = 0;
+    }
 }
+
+function restarTiempo (){
+  tiempo --;
+     document.querySelector('#Tiempo').innerHTML = "tiempo " + tiempo;
+     if (tiempo == 0){
+       alert("Perdiste maestro");
+       tiempo = 60;
+       puntos = 0;
+     }
+     
+}
+
+setInterval(restarTiempo,1000)
 
 
